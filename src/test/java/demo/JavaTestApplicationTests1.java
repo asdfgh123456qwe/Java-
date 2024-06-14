410641e527cf17d4aadc2f705da723a7c616aa18
asdfg123456qwe <1090247102@qq.com>
2024-06-14 17:51:44 +0800

    Java基础练习项目提交

---
+++ src/test/java/demo/JavaTestApplicationTests1.java
@@ -0,0 +1,22 @@
+package demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+@SpringBootTest
+class JavaTestApplicationTests1 {
+
+    /**
+     * 算法练习
+     */
+
+    @Test
+    public void testLambda(){
+
+
+        System.out.println(UUID.randomUUID());
+    }
+
+}
