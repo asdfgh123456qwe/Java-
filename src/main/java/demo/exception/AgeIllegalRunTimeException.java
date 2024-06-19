c23bc7e741f9cc512843a0f0f32b01058e9de305
asdfg123456qwe <1090247102@qq.com>
2024-06-19 10:50:00 +0800

    Java基础练习项目提交

---
+++ src/main/java/demo/exception/AgeIllegalRunTimeException.java
@@ -0,0 +1,15 @@
+package demo.exception;
+
+/**
+ * 年龄异常自定义异常类
+ *
+ * @author LENOVO
+ * @since 2024/6/19 10:25
+ */
+public class AgeIllegalRunTimeException extends RuntimeException{
+    public AgeIllegalRunTimeException(){}
+
+    public AgeIllegalRunTimeException(String message){
+        super(message);
+    }
+}
